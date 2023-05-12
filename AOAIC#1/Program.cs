ea36e65d02194dbe69c3bfd9d233dfcdb549e603
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2023-05-12 19:54:06 +0000

    added appsettings config support

--- AOAIC#1/Program.cs
+++ AOAIC#1/Program.cs
@@ -6,9 +6,12 @@ using Microsoft.Extensions.Configuration.UserSecrets;
 //public static IConfiguration Configuration { get; set; }
 
 var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", true, true)   
     .AddUserSecrets<Program>()
     .Build();
 
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine($"Hello, {config["Bobo"]}");
+Console.WriteLine($"appSettings, {config["AzureKeyVaultEndpoint"]}");
+
